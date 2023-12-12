836bafd96aec89c300502f939b03eb306812ce8f
MAXIM KOSHELEV <maxim.koshelev@aliexpress.ru>
2023-12-12 12:12:28 +0300

    add solution

---
+++ src/arr/transformArrToObj.ts
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// @ts-nocheck
+
+// 12. Напишите функцию, которая сделает из массива объект
+
+const arr = [
+    {name: 'name', value: 'Анатолий'},
+    {name: 'age', value: '40'}
+]
+function transform(arr) {
+    return arr.reduce((obj, item) => {
+            obj[item.name] = item.value
+            return obj
+        }, {})
+}
+
+// function transform(arr) {
+//     const obj = {}
+
+//     for (let item of arr) {
+//         obj[item.name] = item.value
+//     }
+
+//     return obj
+// }
+
+
+console.log(transform(arr)) // {name: 'Анатолий', age: 40}
+
+

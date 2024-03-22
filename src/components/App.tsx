44f867fc9c535e4fd884e52f228fe63ee282f8dc
MAXIM KOSHELEV <maxim.koshelev@aliexpress.ru>
2024-03-22 22:50:01 +0300

    base setup

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+type AppProps = {}
+
+export const App: React.FC<AppProps> = (props) => {
+
+    const {} = props;
+
+    return <div>App</div>;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-  entry: './src/index.ts',
+  entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -41,9 +41,6 @@ const config = {
       '.ts',
       '.js'
     ],
-    alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
   }
 };
 
